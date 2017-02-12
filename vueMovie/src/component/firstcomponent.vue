d3d6ddfba14ee22dae970435d11cadf4e2d34ea4
Bran <307498737@qq.com>
2017-02-12 11:17:48 +0800

    movie

---
+++ vueMovie/src/component/firstcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="firstcomponent">
+		<h1>I'm a title.</h1>
+		<a href=""> written by {{author}} </a>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data() {
+			return{
+				author: "zhinian  fighting ！！！"
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+	
+</style>
